269ed88a8e6827b8b974110bc2124bf7bf013e62
jali-k <e19210@eng.pdn.ac.lk>
2024-02-06 09:07:44 +0530

    Adjust colors

--- lib/screens/res/app_colors.dart
+++ lib/screens/res/app_colors.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color red = Colors.red;
-  static const Color green = Colors.green;
-  static const Color purple = Colors.purple;
+  static const Color red = Color(0xFFE5256A);
+  static const Color green = Color(0xFF00C897);
+  static const Color purple = Color(0xFF9747FF);
   static const Color black = Colors.black;
-  static const Color backGround = Color(0xFFB4B4B4);
+  static const Color backGround = Color(0xFFF4F4F4);
   static const Color ligthWhite = Color(0xFFFAFAFA);
   static const Color blue = Colors.blue;
   static const Color grey = Colors.grey;
